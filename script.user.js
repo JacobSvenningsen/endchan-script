109b3f36bd2a57187fbad813c20baf7833ccb2d3
JacobSvenningsen <jacobsvenningsen15@hotmail.com>
2020-04-08 19:41:19 +0200

    Support for pasting image from clipboard into quickreply

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          endchan-script
-// @version       1.2.5
+// @version       1.2.6
 // @namespace     endchan-script
 // @author        JacobSvenningsen
 // @description   Adds features and fixes functionality of endchan
@@ -694,7 +694,31 @@ function readyFn() {
     if(typeof refreshTimer !== "undefined" && currentRefresh > parseInt(localStorage.getItem("refreshInterval"))) {
       currentRefresh = parseInt(localStorage.getItem("refreshInterval"))
     }
-    if (document.getElementById('postreply')) {jsButton.onclick = function() {postReply(); if(document.getElementById("qrbody")) {qrbody.value = ""}}}
+    if (document.getElementById('postreply')) {
+      jsButton.onclick = function() {
+        postReply(); 
+        if(document.getElementById("qrbody")) {qrbody.value = ""}
+      }
+    }
+
+    //Credit goes to https://stackoverflow.com/a/15369753
+    if (document.getElementById('qrbody')) {
+      qrbody.onpaste = function (event) {
+        // use event.originalEvent.clipboard for newer chrome versions
+        var items = (event.clipboardData  || event.originalEvent.clipboardData).items;
+        // find pasted image among pasted items
+        var blob = null;
+        for (var i = 0; i < items.length; i++) {
+          if (items[i].type.indexOf("image") === 0) {
+            blob = items[i].getAsFile();
+          }
+        }
+        // load image if there is a pasted image
+        if (blob !== null) {
+          addSelectedFile(blob);
+        }
+      }
+    }
     addCounters()
   }
 }
