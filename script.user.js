3de939d684979579dbbdcd00dbac00d83238434f
JacobSvenningsen <jacobsvenningsen15@hotmail.com>
2021-03-11 20:06:01 +0100

    Add mobile check and change behaviour when on mobile

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          endchan-script
-// @version       1.3.10
+// @version       1.4.0
 // @namespace     endchan-script
 // @author        JacobSvenningsen
 // @description   Adds features and fixes functionality of endchan
@@ -13,16 +13,39 @@
 // @downloadURL   https://github.com/JacobSvenningsen/endchan-script/raw/master/script.user.js
 // ==/UserScript==
 
+
 let oldQuotesEventMap = new Map()
 let qrSettings = {}
 let keyPressMap = new Map()
+let onMobile = false;
+
+function mobileStyle() {
+  let style = document.createElement("style")
+  style.id = "fixes_styles"
+  style.type = "text/css"
+  style.innerText = ' \
+    body { \
+        font-size: 18pt !important; \
+    } \
+ \
+    #threadList, \
+    #divThreads { \
+        width:100% !important; \
+    } \
+\
+    #quick-reply .post-table > tbody tr:first-child { \
+        display:none; \
+    } \
+  '
+  return style;
+}
 
 function insertAtCaret(open, close) {
   var startPos = qrbody.selectionStart;
   var endPos = qrbody.selectionEnd;
   var scrollTop = qrbody.scrollTop;
   var marked_text = "";
-  for (var i = qrbody.selectionStart; i < qrbody.selectionEnd; i++) {
+  for (let i = qrbody.selectionStart; i < qrbody.selectionEnd; i++) {
     marked_text += qrbody.value[i]
   }
   qrbody.value = qrbody.value.substring(0, startPos) + open + marked_text + close + qrbody.value.substring(endPos, qrbody.value.length);
@@ -33,7 +56,7 @@ function insertAtCaret(open, close) {
 };
 
 function KeyPress(e) { //Adds quick shortcuts for markup and posting
-  var evtobj = window.event? event : e
+  var evtobj = e
   let entry = (evtobj.ctrlKey ? "C" : "") + (evtobj.altKey ? "A" : "") + (evtobj.shiftKey ? "S" : "") + evtobj.key.toUpperCase();
   let shortcut = keyPressMap.get(entry)
   if (shortcut) {
@@ -311,7 +334,7 @@ function settingsElement(applyHoverImgEvent, window, updateAllLinks) {
     } else {
       if(document.getElementById("threadList")) {
         var imgs = threadList.getElementsByClassName("uploadCell")
-        for (var i = 0; i < imgs.length; i++) {
+        for (let i = 0; i < imgs.length; i++) {
           imgs[i].lastElementChild.onmouseover = null
           imgs[i].lastElementChild.onmouseout = null
         }
@@ -632,8 +655,22 @@ function namefield(window) {
 
 function readyFn() {
   console.log("Running script")
+  
   if (GM) {
     var window = unsafeWindow
+  }
+    processPostingQuote = function(l) {/* discard what happens */}
+  
+  function mobileCheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  }
+  
+  onMobile = mobileCheck();
+  
+  if (onMobile) {
+    document.head.appendChild(mobileStyle());
   }
   
   let uniqueIds;
@@ -668,14 +705,14 @@ function readyFn() {
   namefield(window)
 
   function setLoop(posts) {
-    for (var i = 0; i < posts.length; i++) {
+    for (let i = 0; i < posts.length; i++) {
       posts[i].setAttribute("loop", "true")
     }
   }
 
   function applyHoverImgEvent(eles) {
     if (localStorage.getItem("hover_enabled") == "true") {
-      for (var i = 0; i < eles.length; i++) {
+      for (let i = 0; i < eles.length; i++) {
         eles[i].lastElementChild.onmouseover = mouseoverfunc
         eles[i].lastElementChild.onmouseout = mouseoutfunc
       };
@@ -683,13 +720,13 @@ function readyFn() {
   }
   
   function insertBreak(eles) {
-    for (var i = 0; i < eles.length; i++) {
+    for (let i = 0; i < eles.length; i++) {
       eles[i].before(document.createElement("div"))
     }
   }
   
   function setIdTextColor(eles) {
-    for (var i = 0; i < eles.length; i++) {
+    for (let i = 0; i < eles.length; i++) {
       var colorAsHex = eles[i].innerText
       var rgb = [0,0,0]
       rgb[0] = parseInt(colorAsHex[0] + colorAsHex[1], 16);
@@ -800,8 +837,8 @@ function readyFn() {
   }
   
   function replaceLinkQuoting(nodes) {
-    for(var i = 0; i < nodes.length; i++) {
-      nodes[i].onclick = function(e) {
+    for(let nodeIndex = 0; nodeIndex < nodes.length; nodeIndex++) {
+      nodes[nodeIndex].onclick = function(e) {
         var toQuote = this.innerText;
 
         if (typeof add_quick_reply_quote != "undefined") {
@@ -816,8 +853,8 @@ function readyFn() {
             if (selection.length > 0) {
               pos += 2 // post is surely marked. Add 2 for '>' + '\n'
             }
-            for (let i = 0; i < selection.length-1; i++) { //each additional newline requires position to be moved by 1 more. Newline already part of selection
-              if (selection[i] === '\n') {
+            for (let j = 0; j < selection.length-1; j++) { //each additional newline requires position to be moved by 1 more. Newline already part of selection
+              if (selection[j] === '\n') {
                 pos += 1 // '>'
               }
             }
@@ -825,6 +862,10 @@ function readyFn() {
             qrbody.focus()
             qrbody.selectionStart = pos
             qrbody.selectionEnd = pos
+            if (onMobile) {
+              document.getElementById("quick-reply").style.right = (window.innerWidth - e.clientX - (document.getElementById("quick-reply").getBoundingClientRect().width / 2)).toString() + "px"
+              document.getElementById("quick-reply").style.top = (e.clientY + 24).toString() + "px"
+            }
           }
         }
 
@@ -838,7 +879,7 @@ function readyFn() {
   function setOnclickEvent(clonedNode, origNode) {
     var newPics = clonedNode.getElementsByClassName("uploadCell")
     var origPics = origNode.getElementsByClassName("uploadCell")
-    for(var i = 0; i < newPics.length; i++) {
+    for(let i = 0; i < newPics.length; i++) {
       //newPics[i].lastElementChild.on("click",  this.bind(expandImage(mouseEvent, newPics[i].lastElementChild)))
       newPics[i].lastElementChild.onclick = function(e) {
         //console.log(this)
@@ -926,7 +967,7 @@ function readyFn() {
   function updateLinks(parent, linkStr, updateEvents, updateMap) {
     var links = parent.getElementsByClassName(linkStr)
     if (linkStr == "quoteLink") {
-      for (var i = 0; i < links.length; i++) {
+      for (let i = 0; i < links.length; i++) {
         let d = document.getElementById(links[i].innerText.slice(2).split(" ")[0])
         if (updateMap && !oldQuotesEventMap.has(links[i])) {
           oldQuotesEventMap.set(links[i], [links[i].onmouseenter, links[i].onmouseout, links[i].href])
@@ -942,7 +983,7 @@ function readyFn() {
         }
       }
     } else {
-      for (var i = 0; i < links.length; i++) {
+      for (let i = 0; i < links.length; i++) {
         links[i].childNodes.forEach(function(quote) {
           if (quote.tagName == "A") {
             if (updateMap && !oldQuotesEventMap.has(quote)) {
@@ -980,7 +1021,7 @@ function readyFn() {
   
   function updateNewlyCreatedBacklinks(node, updateLink) {
     var quotes = node.getElementsByClassName("quoteLink")
-    for (var i = 0; i < quotes.length; i++) {
+    for (let i = 0; i < quotes.length; i++) {
       var ele = document.getElementById(quotes[i].innerText.slice(2).split(" ")[0])
       if (ele) {
         var backlinks = ele.getElementsByClassName("panelBacklinks")
@@ -1133,7 +1174,7 @@ function readyFn() {
     setLoop(threadList.getElementsByTagName("video"))
     applyHoverImgEvent(threadList.getElementsByClassName("uploadCell"))
     setIdTextColor(threadList.getElementsByClassName("labelId"))
-    replaceLinkQuoting(threadList.getElementsByClassName("linkQuote"))
+    replaceLinkQuoting(document.getElementsByClassName("linkQuote"))
     updateLinks(threadList, "panelBacklinks", localStorage.getItem("postInlining") == "true", true)
     updateLinks(threadList, "quoteLink", localStorage.getItem("postInlining") == "true", true)
     moveMultipleUploadsClassInit(document.getElementById("divThreads"))
@@ -1156,7 +1197,7 @@ function readyFn() {
         var items = (event.clipboardData  || event.originalEvent.clipboardData).items;
         // find pasted image among pasted items
         var blob = null;
-        for (var i = 0; i < items.length; i++) {
+        for (let i = 0; i < items.length; i++) {
           if (items[i].type.indexOf("image") === 0) {
             blob = items[i].getAsFile();
           }
@@ -1175,6 +1216,22 @@ function readyFn() {
       let oldDragFunc = document.getElementById("quick-reply").getElementsByClassName("handle")[0].onmousedown
       document.getElementById("quick-reply").getElementsByClassName("handle")[0].onmousedown = function(e) {oldDragFunc(e); e.preventDefault(); e.stopPropagation();}
       if (qrSettings.enabled) qrbody.onkeydown = KeyPress
+      
+      if (onMobile) {
+        let qr = document.getElementById("quick-reply");
+        let secondRow = qr.getElementsByClassName("post-table")[0].childNodes[0].childNodes[2];
+        secondRow.appendChild(qremail.parentElement);
+        secondRow.appendChild(qrsubject.parentElement);
+        qr.getElementsByClassName("post-table")[0].childNodes[0].childNodes[4].remove();
+        qr.getElementsByClassName("post-table")[0].childNodes[0].childNodes[3].remove();
+        qremail.parentElement.colSpan = qrname.parentElement.colSpan = qrsubject.parentElement.colSpan = 1;
+        qr.getElementsByClassName("post-table")[0].childNodes[0].childNodes[3].firstChild.colSpan = 3;
+        qr.getElementsByClassName("post-table")[0].childNodes[0].childNodes.forEach(p => { 
+          if(p.nodeName === "TR" && p !== qrname.parentElement.parentElement && p.firstChild.nodeName === "TD") {
+            p.firstChild.colSpan = 3;
+          }
+        });
+      }
     }
     addCounters()
     if (document.getElementById("divPosts")) {
@@ -1182,7 +1239,7 @@ function readyFn() {
       divPosts.childNodes.forEach(hideThisPost)
     } else {
       var eles = document.getElementsByClassName("divPosts")
-      for (var i = 0; i < eles.length; i++) {
+      for (let i = 0; i < eles.length; i++) {
         let children = eles[i].childNodes
         let index = children.length-1;
         while (index >= 0) {
@@ -1250,5 +1307,6 @@ function extraStyles() {
   document.head.appendChild(extraStyles())
   console.log("done injecting css")
 }).call();
+
 
 
